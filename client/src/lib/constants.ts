2685d52efd151bb874f860a6cf2a4db43ff902bf
TALHA-089 <rajag47786@gmail.com>
2026-05-12 09:43:18 +0500

    backend code

--- client/src/lib/constants.ts
+++
@@ -1,1 +0,0 @@
-export const PRODUCT_CATEGORIES = ["Streetwear", "Sneakers", "Accessories", "Tech", "Y2K", "Beauty"];
--- client/src/types/cart.ts
+++
@@ -1,8 +0,0 @@
-export interface CartItem {
-  productId: string;
-  name: string;
-  price: number;
-  imageUrl: string;
-  quantity: number;
-  stock: number;
-}
